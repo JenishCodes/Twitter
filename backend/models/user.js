bccc3066449bd5fe1c9d051ef8dbb7191dc347b7
JenishCodes <padodaraj@gmail.com>
2022-06-02 16:01:33 +0530

    Changed some file in backend folder for deployment

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,86 +4,88 @@ const { ObjectId } = mongoose.SchemaTypes;
 
 const User = mongoose.model(
   "User",
-  new mongoose.Schema({
-    auth_id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    account_name: {
-      type: String,
-      required: true,
-      maxlength: 15,
-      unique: true,
-    },
-    name: {
-      type: String,
-      required: true,
-      maxlength: 50,
-    },
-    description: {
-      type: String,
-      maxlength: 256,
-      default: "",
-    },
-    pinned_tweet_id: {
-      type: ObjectId,
-      ref: "Tweet",
-      default: null,
-    },
-    bookmarks: [
-      {
+  new mongoose.Schema(
+    {
+      auth_id: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      account_name: {
+        type: String,
+        required: true,
+        maxlength: 15,
+        unique: true,
+      },
+      name: {
+        type: String,
+        required: true,
+        maxlength: 50,
+      },
+      description: {
+        type: String,
+        maxlength: 256,
+        default: "",
+      },
+      pinned_tweet_id: {
         type: ObjectId,
         ref: "Tweet",
-        _id: false,
+        default: null,
       },
-    ],
-    entities: {
-      hashtags: [
+      bookmarks: [
         {
-          start: Number,
-          end: Number,
-          tag: String,
+          type: ObjectId,
+          ref: "Tweet",
+          _id: false,
         },
       ],
-      urls: [
-        {
-          start: Number,
-          end: Number,
-          url: String,
-        },
-      ],
-      mentions: [
-        {
-          start: Number,
-          end: Number,
-          tag: String,
-        },
-      ],
+      entities: {
+        hashtags: [
+          {
+            start: Number,
+            end: Number,
+            tag: String,
+          },
+        ],
+        urls: [
+          {
+            start: Number,
+            end: Number,
+            url: String,
+          },
+        ],
+        mentions: [
+          {
+            start: Number,
+            end: Number,
+            tag: String,
+          },
+        ],
+      },
+      website: {
+        type: String,
+        default: "",
+      },
+      location: {
+        type: String,
+        default: "",
+      },
+      profile_image_url: {
+        type: String,
+        default:
+          "https://firebasestorage.googleapis.com/v0/b/twitter-webapp-clone.appspot.com/o/profile_images%2Fprofile_images%2Fdefault_profile_image.jpg?alt=media&token=7b63b3aa-9eb4-42d4-99ed-7b88bf097404",
+      },
+      banner_image_url: {
+        type: String,
+        default: "",
+      },
+      followers_count: { type: Number, default: 0 },
+      following_count: { type: Number, default: 0 },
+      tweets_count: { type: Number, default: 0 },
+      favourites_count: { type: Number, default: 0 },
     },
-    website: {
-      type: String,
-      default: "",
-    },
-    location: {
-      type: String,
-      default: "",
-    },
-    profile_image_url: {
-      type: String,
-      default:
-        "https://firebasestorage.googleapis.com/v0/b/micro-blogging-web.appspot.com/o/profile_images%2Fdefault_user_image.png?alt=media&token=b51f1b08-2063-40a8-9fb1-2ee8654404fc",
-    },
-    banner_image_url: {
-      type: String,
-      default:
-        "https://firebasestorage.googleapis.com/v0/b/micro-blogging-web.appspot.com/o/banner_images%2Fdefault.png?alt=media&token=912425e2-c409-41bc-ae19-a7cf852cb513",
-    },
-    followers_count: { type: Number, default: 0 },
-    following_count: { type: Number, default: 0 },
-    tweets_count: { type: Number, default: 0 },
-    favourites_count: { type: Number, default: 0 },
-  }, {timestamps: true})
+    { timestamps: true }
+  )
 );
 
 exports.User = User;
